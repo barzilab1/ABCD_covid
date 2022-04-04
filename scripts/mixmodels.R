7349e16a945d04c94fce3c86f8364fd31dc61a3d
trant13@chop.edu <trant13@chop.edu>
2022-04-04 01:33:08 -0400

    Fix age variable.

--- scripts/mixmodels.R
+++ scripts/mixmodels.R
@@ -17,27 +17,49 @@ covidy_long <- read.csv("outputs/covidy_long.csv")
 family_ <- read.csv("outputs/family.csv") %>% dplyr::select(-eventname)
 demo_baseline <- read.csv("outputs/demographics_baseline.csv")
 p_factor_with_sui <- read_csv(paste0(p_factor_files_path, "ABCD_psychopathology_bifactor_scores_23March2021_WITH_SUICIDALITY.csv"))
+demographics_long <- read.csv("../outputs/demographics_long.csv")
+exposome_sum_set <- read.csv("../outputs/exposome_sum_set.csv")
 site <- read.csv("outputs/site.csv") %>% 
   dplyr::filter(eventname == "1_year_follow_up_y_arm_1") %>% 
   dplyr::select(src_subject_id, site_id_l_br)
-
-demo_baseline[,c("interview_date" ,"interview_age" ,"eventname" )] <- NULL
 
 p_factor_with_sui$src_subject_id <- paste0("NDAR_", p_factor_with_sui$ID)
 p_factor_with_sui$ID <- NULL
 colnames(p_factor_with_sui)[1:7] <- paste0(colnames(p_factor_with_sui)[1:7], "_with_sui")
 
+IVs <- merge(demographics_long, exposome_sum_set, all = T)
+IVs <- IVs[(IVs$eventname == "1_year_follow_up_y_arm_1"),]
 
-dataset <- merge(covidp_long,covidy_long, all = T )
-dataset <- merge(dataset,family_, all.x = T )
-dataset <- merge(dataset,demo_baseline, all.x = T )
+colnames(IVs)[2] <- "interview_date_before_covid"
+colnames(IVs)[3] <- "interview_age_before_covid"
+IVs$eventname <- NULL
 
+dataset <- merge(covidp_long, covidy_long, all = T )
+dataset <- merge(dataset, family_, all.x = T )
+dataset <- merge(dataset, IVs)
+
+demo_baseline <- demo_baseline %>%
+  dplyr::select(
+    -sex,
+    -household_income,
+    -parents_avg_edu,
+    -separated_or_divorced,
+    -parents_married,
+    -age,
+    -sex_br,
+    -gender,
+    -demo_fam_poverty,
+    -interview_date,
+    -interview_age,
+    -eventname
+  )
+
+dataset <- merge(dataset, demo_baseline, all.x = T)
+dataset <- merge(dataset, p_factor_with_sui, all.x = T)
+dataset <- merge(dataset, site, all.x = T)
 
 dataset$rel_family_id <- dataset$rel_family_id + 1
 dataset$timepoint <- sub("cv", "" ,dataset$timepoint )
-
-dataset <- merge(dataset, p_factor_with_sui, all.x = T)
-dataset <- merge(dataset, site, all.x = T)
 
 # Add columns
 dataset$morning_bedtime_routine <- rowMeans(dataset[, grep("routine", colnames(dataset))], na.rm = T)
@@ -94,23 +116,25 @@ dataset <- dataset %>%
     as.numeric
   ))
 
+# writexl::write_xlsx(dataset, "outputs/mixedmodels_dat_final.xlsx")
+
 ################### NO INTERACTIONS ################### 
 # OUTCOME 1: WAGE LOSS
 ## Model 1 (+ pre-covid SES)
 model_1_DV <- lmer(scale(felt_sad_cv_raw_tot_bar) ~ fam_wage_loss_cv + timepoint + 
-                        scale(age) + sex_br + race_white + race_black + ethnicity_hisp + scale(household_income) + scale(parents_avg_edu) +
+                        scale(interview_age) + sex_br + race_white + race_black + ethnicity_hisp + scale(household_income) + scale(parents_avg_edu) +
                       (1 | rel_family_id/src_subject_id), 
                       data = dataset, verbose = FALSE)
 
 ## Model 2 (+ P-factor) [MAIN MODEL]
 model_2_DV <- lmer(scale(felt_sad_cv_raw_tot_bar) ~ fam_wage_loss_cv + timepoint + 
-                        scale(age) + sex_br + race_white + race_black + ethnicity_hisp + scale(household_income) + scale(parents_avg_edu) + General_p_with_sui +
+                        scale(interview_age) + sex_br + race_white + race_black + ethnicity_hisp + scale(household_income) + scale(parents_avg_edu) + General_p_with_sui +
                       (1 | rel_family_id/src_subject_id), 
                       data = dataset, verbose = FALSE)
 
 ## Model 3 (+ exposome)
 model_3_DV <- lmer(scale(felt_sad_cv_raw_tot_bar) ~ fam_wage_loss_cv + timepoint + 
-                        scale(age) + sex_br + race_white + race_black + ethnicity_hisp + scale(household_income) + scale(parents_avg_edu) + General_p_with_sui +
+                        scale(interview_age) + sex_br + race_white + race_black + ethnicity_hisp + scale(household_income) + scale(parents_avg_edu) + General_p_with_sui +
                         scale(morning_bedtime_routine) + scale(screentime_wknd_hr_cv) + scale(demo_exercise_cv) + scale(parent_monitor_tot_bar) + scale(increased_conflict_cv) + child_tested_pos_cv + 
                         scale(fam_isolate_tot_cv) + child_separate_cv + school_close_spring_2020_cv + #went_to_school_cv #enjoy_school_y_cv not work
                       (1 | rel_family_id/src_subject_id), 
@@ -121,24 +145,24 @@ comb_1
 
 # OUTCOME 2: FINANCIAL WORRIES
 model_1_fw_DV <- lmer(scale(felt_sad_cv_raw_tot_bar) ~ Residualized_money_cv + timepoint + 
-                           scale(age) + sex_br + race_white + race_black + ethnicity_hisp + scale(household_income) + scale(parents_avg_edu) +
+                           scale(interview_age) + sex_br + race_white + race_black + ethnicity_hisp + scale(household_income) + scale(parents_avg_edu) +
                          (1 | rel_family_id/src_subject_id), 
                          data = dataset, verbose = FALSE)
 
 model_2_fw_DV <- lmer(scale(felt_sad_cv_raw_tot_bar) ~ Residualized_money_cv + timepoint + 
-                           scale(age) + sex_br + race_white + race_black + ethnicity_hisp + scale(household_income) + scale(parents_avg_edu) + General_p_with_sui +
+                           scale(interview_age) + sex_br + race_white + race_black + ethnicity_hisp + scale(household_income) + scale(parents_avg_edu) + General_p_with_sui +
                          (1 | rel_family_id/src_subject_id), 
                          data = dataset, verbose = FALSE)
 
 model_3_fw_DV <- lmer(scale(felt_sad_cv_raw_tot_bar) ~ Residualized_money_cv + timepoint + 
-                           scale(age) + sex_br + race_white + race_black + ethnicity_hisp + scale(household_income) + scale(parents_avg_edu) + General_p_with_sui +
+                           scale(interview_age) + sex_br + race_white + race_black + ethnicity_hisp + scale(household_income) + scale(parents_avg_edu) + General_p_with_sui +
                            scale(morning_bedtime_routine) + scale(screentime_wknd_hr_cv) + scale(demo_exercise_cv) + scale(parent_monitor_tot_bar) + scale(increased_conflict_cv) +
                            child_tested_pos_cv + scale(fam_isolate_tot_cv) + child_separate_cv + school_close_spring_2020_cv +
                          (1 | rel_family_id/src_subject_id), 
                          data = dataset, verbose = FALSE)
 
 model_4_fw_DV <- lmer(scale(felt_sad_cv_raw_tot_bar) ~ Residualized_money_cv + timepoint + fam_wage_loss_cv +
-                           scale(age) + sex_br + race_white + race_black + ethnicity_hisp + scale(household_income) + scale(parents_avg_edu) + General_p_with_sui +
+                           scale(interview_age) + sex_br + race_white + race_black + ethnicity_hisp + scale(household_income) + scale(parents_avg_edu) + General_p_with_sui +
                            scale(morning_bedtime_routine) + scale(screentime_wknd_hr_cv) + scale(demo_exercise_cv) + scale(parent_monitor_tot_bar) + scale(increased_conflict_cv) +
                            child_tested_pos_cv + scale(fam_isolate_tot_cv) + child_separate_cv + school_close_spring_2020_cv +
                          (1 | rel_family_id/src_subject_id), 
@@ -152,7 +176,7 @@ comb_2
 # OUTCOME 1: WAGE LOSS
 ## scale(household_income)*fam_wage_loss_cv
 model_1_DV_int <- lmer(scale(felt_sad_cv_raw_tot_bar) ~ fam_wage_loss_cv + timepoint + scale(household_income)*fam_wage_loss_cv +
-                            scale(age) + sex_br + race_white + race_black + ethnicity_hisp + scale(parents_avg_edu) + General_p_with_sui +
+                            scale(interview_age) + sex_br + race_white + race_black + ethnicity_hisp + scale(parents_avg_edu) + General_p_with_sui +
                           (1 | rel_family_id/src_subject_id), 
                           data = dataset, verbose = FALSE)
 
@@ -162,7 +186,7 @@ comb_1_int
 # OUTCOME 2: FINANCIAL WORRIES
 ## scale(household_income) * Residualized_money_cv
 model_1_fw_DV_int <- lmer(scale(felt_sad_cv_raw_tot_bar) ~ Residualized_money_cv + timepoint + scale(household_income)*Residualized_money_cv +
-                               scale(age) + sex_br + race_white + race_black + ethnicity_hisp + scale(parents_avg_edu) + General_p_with_sui +
+                               scale(interview_age) + sex_br + race_white + race_black + ethnicity_hisp + scale(parents_avg_edu) + General_p_with_sui +
                              (1 | rel_family_id/src_subject_id), 
                              data = dataset, verbose = FALSE)
 
@@ -173,12 +197,12 @@ comb_2_int
 
 # OUTCOME 3: mental_health_cv
 model_1_mh_DV <- lmer(scale(mental_health_cv) ~ fam_wage_loss_cv + timepoint + 
-                           scale(age) + sex_br + race_white + race_black + ethnicity_hisp + scale(household_income) + scale(parents_avg_edu) + General_p_with_sui +
+                           scale(interview_age) + sex_br + race_white + race_black + ethnicity_hisp + scale(household_income) + scale(parents_avg_edu) + General_p_with_sui +
                          (1 | rel_family_id/src_subject_id), 
                          data = dataset, verbose = FALSE)
 
 model_2_mh_DV <- lmer(scale(mental_health_cv) ~ Residualized_money_cv + timepoint + 
-                           scale(age) + sex_br + race_white + race_black + ethnicity_hisp + scale(household_income) + scale(parents_avg_edu) + General_p_with_sui +
+                           scale(interview_age) + sex_br + race_white + race_black + ethnicity_hisp + scale(household_income) + scale(parents_avg_edu) + General_p_with_sui +
                          (1 | rel_family_id/src_subject_id), 
                          data = dataset, verbose = FALSE)
 
@@ -187,12 +211,12 @@ comb_3
 
 # OUTCOME 4: su_total_cv
 model_1_su_DV <- lmer(scale(su_total_cv) ~ fam_wage_loss_cv + timepoint + 
-                           scale(age) + sex_br + race_white + race_black + ethnicity_hisp + scale(household_income) + scale(parents_avg_edu) + General_p_with_sui +
+                           scale(interview_age) + sex_br + race_white + race_black + ethnicity_hisp + scale(household_income) + scale(parents_avg_edu) + General_p_with_sui +
                          (1 | rel_family_id/src_subject_id), 
                          data = dataset, verbose = FALSE)
 
 model_2_su_DV <- lmer(scale(su_total_cv) ~ Residualized_money_cv + timepoint + 
-                           scale(age) + sex_br + race_white + race_black + ethnicity_hisp + scale(household_income) + scale(parents_avg_edu) + General_p_with_sui +
+                           scale(interview_age) + sex_br + race_white + race_black + ethnicity_hisp + scale(household_income) + scale(parents_avg_edu) + General_p_with_sui +
                          (1 | rel_family_id/src_subject_id), 
                          data = dataset, verbose = FALSE)
 
@@ -201,12 +225,12 @@ comb_4
 
 # OUTCOME 5: pstr_cv_raw_tot_bar
 model_1_pstr_DV <- lmer(scale(pstr_cv_raw_tot_bar) ~ fam_wage_loss_cv + timepoint + 
-                             scale(age) + sex_br + race_white + race_black + ethnicity_hisp + scale(household_income) + scale(parents_avg_edu) + General_p_with_sui +
+                             scale(interview_age) + sex_br + race_white + race_black + ethnicity_hisp + scale(household_income) + scale(parents_avg_edu) + General_p_with_sui +
                            (1 | rel_family_id/src_subject_id), 
                            data = dataset, verbose = FALSE)
 
 model_2_pstr_DV <- lmer(scale(pstr_cv_raw_tot_bar) ~ Residualized_money_cv + timepoint + 
-                             scale(age) + sex_br + race_white + race_black + ethnicity_hisp + scale(household_income) + scale(parents_avg_edu) + General_p_with_sui +
+                             scale(interview_age) + sex_br + race_white + race_black + ethnicity_hisp + scale(household_income) + scale(parents_avg_edu) + General_p_with_sui +
                            (1 | rel_family_id/src_subject_id), 
                            data = dataset, verbose = FALSE)
 
@@ -215,12 +239,12 @@ comb_5
 
 # EXPOSURES
 model_4_DV <- lmer(scale(felt_sad_cv_raw_tot_bar) ~ fam_exp_tot_cv + timepoint + 
-                        scale(age) + sex_br + race_white + race_black + ethnicity_hisp + scale(household_income) + scale(parents_avg_edu) + General_p_with_sui +
+                        scale(interview_age) + sex_br + race_white + race_black + ethnicity_hisp + scale(household_income) + scale(parents_avg_edu) + General_p_with_sui +
                       (1 | rel_family_id/src_subject_id), 
                       data = dataset, verbose = FALSE)
 
 model_5_DV <- lmer(scale(felt_sad_cv_raw_tot_bar) ~ child_worried_about_cv + timepoint + 
-                        scale(age) + sex_br + race_white + race_black + ethnicity_hisp + scale(household_income) + scale(parents_avg_edu) + General_p_with_sui +
+                        scale(interview_age) + sex_br + race_white + race_black + ethnicity_hisp + scale(household_income) + scale(parents_avg_edu) + General_p_with_sui +
                       (1 | rel_family_id/src_subject_id), 
                       data = dataset, verbose = FALSE)
 
@@ -232,12 +256,12 @@ comb_6
 
 ## Model 2 (+ P-factor) [MAIN MODEL] + SITE
 model_2_DV_site <- lmer(scale(felt_sad_cv_raw_tot_bar) ~ fam_wage_loss_cv + timepoint + 
-                             scale(age) + sex_br + race_white + race_black + ethnicity_hisp + scale(household_income) + scale(parents_avg_edu) + 
+                             scale(interview_age) + sex_br + race_white + race_black + ethnicity_hisp + scale(household_income) + scale(parents_avg_edu) + 
                           General_p_with_sui + site_id_l_br + (1 | rel_family_id/src_subject_id), 
                            data = dataset, verbose = FALSE)
 
 model_2_DV_rsite <- lmer(scale(felt_sad_cv_raw_tot_bar) ~ fam_wage_loss_cv + timepoint + 
-                           scale(age) + sex_br + race_white + race_black + ethnicity_hisp + scale(household_income) + scale(parents_avg_edu) + 
+                           scale(interview_age) + sex_br + race_white + race_black + ethnicity_hisp + scale(household_income) + scale(parents_avg_edu) + 
                            General_p_with_sui + (1 | rel_family_id/src_subject_id) + (1 | site_id_l_br), 
                          data = dataset, verbose = FALSE)
 
@@ -246,12 +270,12 @@ comb_1_site
 
 # OUTCOME: FINANCIAL WORRIES
 model_2_fw_DV_site <- lmer(scale(felt_sad_cv_raw_tot_bar) ~ Residualized_money_cv + timepoint + 
-                                scale(age) + sex_br + race_white + race_black + ethnicity_hisp + scale(household_income) + scale(parents_avg_edu) + 
+                                scale(interview_age) + sex_br + race_white + race_black + ethnicity_hisp + scale(household_income) + scale(parents_avg_edu) + 
                              General_p_with_sui + site_id_l_br + (1 | rel_family_id/src_subject_id), 
                               data = dataset, verbose = FALSE)
 
 model_2_fw_DV_rsite <- lmer(scale(felt_sad_cv_raw_tot_bar) ~ Residualized_money_cv + timepoint + 
-                              scale(age) + sex_br + race_white + race_black + ethnicity_hisp + scale(household_income) + scale(parents_avg_edu) + 
+                              scale(interview_age) + sex_br + race_white + race_black + ethnicity_hisp + scale(household_income) + scale(parents_avg_edu) + 
                               General_p_with_sui + (1 | rel_family_id/src_subject_id) + (1 | site_id_l_br), 
                             data = dataset, verbose = FALSE)
 
